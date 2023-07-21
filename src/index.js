0a0430f400f2566498587febe571e72745bd2126
Coder.Play <31927031+tshepo-desh-mathabe@users.noreply.github.com>
2023-07-21 14:13:51 +0000

    init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "semantic-ui-css/semantic.min.css";
+import history from "./utils/BrowserHistory";
+import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
 
 import App from "./App";
 
@@ -8,6 +11,8 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <HistoryRouter history={history}>
+      <App />
+    </HistoryRouter>
   </StrictMode>
 );
---
+++ src/utils/app_routes/AppRoutes.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import PATH from "../constants/route_path.json";
+import * as page from "../../components/index";
+
+const AppRoutes = () => {
+  const routes = useRoutes([
+    { path: PATH.home, element: <page.Home /> },
+    // { path: PATH.signIn, element: <page.Login /> },
+    // { path: PATH.home, element: <page.Home /> },
+    // { path: PATH.defect, element: <page.Defect /> },
+    // { path: PATH.changeRequest, element: <page.ChangeRequest /> },
+    // { path: PATH.userStory, element: <page.UserStory /> },
+    // { path: PATH.other, element: <page.Other /> },
+    // { path: PATH.allRoutes, element: <page.Home /> }
+  ]);
+
+  return routes;
+};
+
+export default AppRoutes;
